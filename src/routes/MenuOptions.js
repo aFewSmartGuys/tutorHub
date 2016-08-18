ea3e82158685c36fa00af4aaf913ed7606ce7187
Santiago Verdu <santiagoverdu@protonmail.com>
2016-08-18 17:00:42 -0600

    menuoptions comments

--- src/routes/MenuOptions.js
+++ src/routes/MenuOptions.js
@@ -1,21 +1,34 @@
 'use strict';
 /**
+ * args:
+ * default boolean Whether to include the authlvl links
+ * homepage boolean whether to include the scroll links
+ * custom Array[opt] custom menu options
+ * dropdowns Array[] | dropdown {} custom dropdown menu or menus
  *
- * args: custom:[], dropdowns[{title:"",opts:[]}] || dropdown {titld:"",opts:[]}, homepage:bool
- *
- *
- *
- *
+ * opt={
+ * 	href:str,
+ * 	value:str
+ * }
+ * dropodown={
+ * 	title:str,
+ * 	opts:Array[opt]
+ * }
  */
 function MenuOptions(args) {
 	this.l3 = [{value:'Dashboard',href:'/admin'}];
 	this.l0 = [{value:'Booking',href:'/user'}];
 
-	// dropdowns if an array is provided otherwise looks for a single object
+	this.homepage = args.homepage || false;
+	this.default = args.default;
 	this.dropdowns = null;
 	this.dropdown = null;
 
+	this.links = args.custom instanceof Array ?
+	args.custom.concat(this.genCustomOpts(args.authLvl)) : this.genCustomOpts(args.authLvl);
+
 	// set up dropdown menu items
+	// dropdowns if an array is provided otherwise looks for a single object
 	if (args.dropdowns instanceof Array) {
 		this.dropdowns = args.dropdowns;
 	} else if (typeof args.dropdowns === 'object') {
@@ -24,10 +37,6 @@ function MenuOptions(args) {
 		// needs to be tested
 		this.dropdown = reqProps.every(hasProp(args.dropdowns))?args.dropdowns:null;
 	}
-
-	this.homepage = args.homepage || false;
-	this.links = args.custom instanceof Array ?
-		args.custom.concat(this.genCustomOpts(args.authLvl)) : this.genCustomOpts(args.authLvl);
 }
 
 MenuOptions.prototype.generateDropdowns = function() {
@@ -42,7 +51,7 @@ MenuOptions.prototype.generateDropdowns = function() {
 }
 
 MenuOptions.prototype.generateLinks = function() {
-	var htmlStr = "";
+	var htmlStr = '';
 	this.links.forEach(function(opt) {
 		htmlStr += "<li><a href='" + opt.href + "'>" + opt.value + "</a></li>";
 	});
@@ -63,7 +72,7 @@ MenuOptions.prototype.genCustomOpts = function(authLvl) {
 	}
 	var optsarr = [];
 	for (let i = 0; i <= authLvl; ++i) {
-		let opts = "l"+i;
+		let opts = 'l'+i;
 		if (this.hasOwnProperty(opts)) {
 			optsarr = optsarr.concat(this[opts]);
 		}
@@ -86,8 +95,8 @@ function createDropdown(dd) {
 			return beg+end;
 		}
 	} else {
-		console.log("Missign properties for dropdown");
-		return "";
+		console.log('Missing properties for dropdown');
+		return '';
 	};
 }
 
